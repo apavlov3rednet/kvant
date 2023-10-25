1fc756c69ae4a1b4d73c05bcedc3b4182228379d
Александр Павлов <a.pavlov@3red.net>
2023-10-25 14:37:41 +0300

    25.10

--- group8/script.js
+++ group8/script.js
@@ -1,29 +1,33 @@
 'use strict';
 
 (function() {
-    const body = document.body;
-    const arLabel = body.querySelectorAll('.service');
-    const arResInt = {
-        'ENGINE_CLEAR' : 0.12,
-        'ENGINE_OWN' : 0.40
-    };
+    const body = document.body; //document.querySelector('body');
+    const arLabel = body.querySelectorAll('label');
+    // const arModify = {
+    //     'ENGINE_CLEAR' : 0.12,
+    //     'ENGINE_OWN' : 0.09
+    // }
 
     let arRes = {};
+
+    let obResult = document.getElementById('result'); //только от document
     let upPrice = 0; //наценка
-    let obResult = body.querySelector("#result"); // document.getElementById('result')
 
-    arLabel.forEach(item => {
+    console.log(arLabel);
 
-        //выбираем поле для обработки
-        let obField = item.querySelector('input[type=checkbox], input[type=text]');
+    arLabel.forEach(function(item) {
+        let field = item.querySelector('[type=text], [type=checkbox]');
+        let r = 0;
+        let result = 0;
 
-        //Если тип поля чекбокс
-        if(obField.type === 'checkbox') {
-            obField.addEventListener('click', function() {
-                let code = obField.name;
-                let value = arResInt[code];
+        //chekboxes
+        if(field.type === 'checkbox') {
+            field.addEventListener('click', function(event) {
+                // let valueCode = event.target.name; //field.name
+                // let value = arModify[valueCode];
+                let value = parseFloat(field.dataset.value);
 
-                if(obField.checked) { //если чекбокс выбран то мы наращиваем наценку
+                if(field.checked) {
                     upPrice = upPrice + value;
                 }
                 else {
@@ -32,25 +36,52 @@
             });
         }
 
-        //Если тип поля текст
-        if(obField.type === 'text') {
-            obField.addEventListener('keyup', function() {
-                arRes[obField.name] = parseFloat(obField.value);
+        //text fields
+        if(field.type === 'text') {
+            field.addEventListener('keyup', e => {
+                let regExr = new RegExp('^[0-9]{1,}$');
+                let arCheckboxes = body.querySelectorAll('[type=checkbox]');
+
+                if(!field.value.match(regExr)) {
+                    item.style.color = "red";
+                    item.style.fontWeight = 'bold';
+                    item.style.borderBottom = '1px dashed red';
+
+                    console.log(item.style);
+
+                    arCheckboxes.forEach(checkbox => {
+                        checkbox.setAttribute('disabled', true);
+                    });
+                }
+                else {
+                    //item.style.color = "black";
+                    item.removeAttribute('style');
+                    arCheckboxes.forEach(checkbox => {
+                        checkbox.removeAttribute('disabled'); //setAttribute('disabled', false)
+                    });
+                }
+
+                arRes[field.name] = parseFloat(field.value);
             });
         }
 
-        //Глобальное условие изменения всех полей, общий пересчет и вывод в результат
-        obField.addEventListener('change', () => {
+        field.addEventListener('change', ()=>{
             let up = (upPrice > 0) ? upPrice + 1 : 1;
+            let res = 0;
             let result = 0;
 
             for(let i in arRes) {
-                result += arRes[i];
+                res += arRes[i];
             }
 
-            obResult.innerText = result * up;
+            result = (res * up).toLocaleString() + ' руб.'; //3000 | 3 000 000 000
+
+            let totalResult = document.createElement('b');
+            totalResult.innerText = result;
+
+            obResult.innerText = 'Полная стоимость: '; //removeContent, obResult.innerHTML
+            obResult.append(totalResult);
         });
 
     });
-
 })(window);
