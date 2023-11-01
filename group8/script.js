efb5b891636e169232d8b56503d426984981535f
apavlov3rednet <92976265+apavlov3rednet@users.noreply.github.com>
2023-11-01 17:55:38 +0300

    Update script.js

--- group8/script.js
+++ group8/script.js
@@ -1,87 +1,76 @@
 'use strict';
 
-(function() {
-    const body = document.body; //document.querySelector('body');
-    const arLabel = body.querySelectorAll('label');
-    // const arModify = {
-    //     'ENGINE_CLEAR' : 0.12,
-    //     'ENGINE_OWN' : 0.09
-    // }
+function Calc() {
+    return {
+        totalPrice: 0,
+        upPrice: 0,
 
-    let arRes = {};
+        Init: function() {
+            this.obBody = document.body;
+            this.arInputsUp = this.obBody.querySelectorAll('[type=checkbox]');
+            this.obField = this.obBody.querySelector('[type=text]');
+            this.obResult = document.getElementById('result');
+            this.arFields = this.obBody.querySelectorAll('input');
+            this.InitDefault();
 
-    let obResult = document.getElementById('result'); //только от document
-    let upPrice = 0; //наценка
-
-    console.log(arLabel);
-
-    arLabel.forEach(function(item) {
-        let field = item.querySelector('[type=text], [type=checkbox]');
-        let r = 0;
-        let result = 0;
-
-        //chekboxes
-        if(field.type === 'checkbox') {
-            field.addEventListener('click', function(event) {
-                // let valueCode = event.target.name; //field.name
-                // let value = arModify[valueCode];
-                let value = parseFloat(field.dataset.value);
-
-                if(field.checked) {
-                    upPrice = upPrice + value;
-                }
-                else {
-                    upPrice -= value;
-                }
+            this.arFields.forEach(item => {
+                let self = this;
+                self.bindEvent(item, 'change');
+                self.bindEvent(item, 'keyup');
             });
-        }
 
-        //text fields
-        if(field.type === 'text') {
-            field.addEventListener('keyup', e => {
-                let regExr = new RegExp('^[0-9]{1,}$');
-                let arCheckboxes = body.querySelectorAll('[type=checkbox]');
+        },
 
-                if(!field.value.match(regExr)) {
-                    item.style.color = "red";
-                    item.style.fontWeight = 'bold';
-                    item.style.borderBottom = '1px dashed red';
+        InitDefault: function() {
+            this.obField.value = 1000;
+            this.reCalc();
+        },
 
-                    console.log(item.style);
+        bindEvent: function(item, e) {
+            let self = this;
+            let regExr = new RegExp('^[0-9]{1,}$');
 
-                    arCheckboxes.forEach(checkbox => {
-                        checkbox.setAttribute('disabled', true);
-                    });
-                }
-                else {
-                    //item.style.color = "black";
-                    item.removeAttribute('style');
-                    arCheckboxes.forEach(checkbox => {
-                        checkbox.removeAttribute('disabled'); //setAttribute('disabled', false)
-                    });
-                }
-
-                arRes[field.name] = parseFloat(field.value);
+            item.addEventListener(e, function(event) {
+                if(event === 'keyup') 
+                    self.showError(item, !item.value.match(regExr));
+                self.reCalc();
             });
-        }
+        },
 
-        field.addEventListener('change', ()=>{
-            let up = (upPrice > 0) ? upPrice + 1 : 1;
-            let res = 0;
-            let result = 0;
-
-            for(let i in arRes) {
-                res += arRes[i];
+        showError: function(item, isset = false) {
+            if(isset) {
+                item.parentNode.style.color = "red";
+                item.parentNode.style.fontWeight = 'bold';
+                item.parentNode.style.borderBottom = '1px dashed red';
             }
+            else {
+                item.parentNode.removeAttribute('style');
+            }
+        },
 
-            result = (res * up).toLocaleString() + ' руб.'; //3000 | 3 000 000 000
+        getUpPrice: function() {
+            let result = 0;
+            this.arInputsUp.forEach(item => {
+                if(item.checked) 
+                    result += parseFloat(item.dataset.value);
+            });
+
+            return result + 1;
+        },
+
+        reCalc: function() {
+            let upCheckbox = this.getUpPrice();
+            let resField = parseFloat(this.obField.value);
+            let result = (resField * upCheckbox).toLocaleString() + ' руб.';
 
             let totalResult = document.createElement('b');
             totalResult.innerText = result;
 
-            obResult.innerText = 'Полная стоимость: '; //removeContent, obResult.innerHTML
-            obResult.append(totalResult);
-        });
+            this.obResult.innerText = 'Полная стоимость: '; //removeContent, obResult.innerHTML
+            this.obResult.append(totalResult);
+        }
+    }
+}
 
-    });
-})(window);
+let calc = Calc();
+calc.Init();
