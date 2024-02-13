65aba1704ad27a036689d8f40a8fb5b99b13fa73
alexxhub <a.pavlov@3red.net>
2024-02-13 10:34:55 +0300

    1

--- index.js
+++ index.js
@@ -1,82 +1,31 @@
-//Подключаем модуль поддержки протокла HTTP
-const http = require('http');
-//Подключаем модуль управления файлами
-const fs = require('fs'); //file system
-const path = require('path');
-const express = require('express');
+const React = require('react');
+const ReactDOM = require('react-dom');
 
-const PORT = 8082;
+//Топорный функционал JSX
+ReactDOM.render(<h1>Привет</h1>, document.getElementById('app'));
 
-//Майм типы данных и расширения файлов
-const mymeType = {
-    '.html' : 'text/html',
-    '.tmpl' : 'text/html',
-    '.js' : 'text/javascript',
-    '.css' : 'text/css',
-    '.png' : 'image/png',
-    '.jpg' : 'image/jpg',
-    '.jpeg' : 'image/jpg',
-    '.gif' : 'image/gif',
-    '.svg' : 'image/svg+xml',
-    '.woff' : 'application/font-woff',
-    '.eot' : 'application/vnd.ms-fontobject',
-    '.ttf' : 'application/font-ttf'
-};
+//Сложное создание без использования JSX
+ReactDOM.render(React.createElement('input', {
+    placeholder: 'test',
+    type: 'text',
+    onClick: () => {} //обязательно camelCase
+}), document.getElementById('app'));
 
-const staticFile = (res, filePath, ext) => {
-    let params = {};
-    if(mymeType[ext].match('text/')) {
-        params = {encoding: 'utf8', flag: 'r'}; 
-    }
+//jsx
+ReactDOM.render(<input placeholder='test' type='text'/>, document.getElementById('app'));
 
-    fs.readFile('.' + filePath, params, (err, data) => {
-        if(err) {
-            console.log(err);
-            res.statusCode = 404;
-            res.end();
-        }
 
-        res.setHeader('Content-Type', mymeType[ext]);
-        res.end(data);
-    });
+//normalize
+const inputClick = () => {
+    console.log('click');
 }
 
-const server = http.createServer(function(req, res) {
-    console.log('Server request');
+const app = document.getElementById('app');
+const element = <input placeholder='test' type='text' onClick={inputClick}/>;
 
-    const createPath = (page) => path.resolve(__dirname, 'views', `${page}.tmpl`);
-    const url = req.url;
+ReactDOM.render(element, app);
 
-    let basePath = '';
-
-    switch(url) {
-        case '/':
-            basePath = '/index.html';
-        break;
-
-        case '/brands':
-            basePath = createPath('brands');
-        break;
-
-        default: 
-            const extname = String(path.extname(url)).toLowerCase();
-            if(extname in mymeType) {
-                console.log(url);
-                console.log(extname);
-                staticFile(res, url, extname);
-            }
-            else {
-                console.log('404');
-                res.end();
-            }
-        break;
-    }
-
-    if(basePath != '') {
-        staticFile(res, basePath, '.html');
-    }
-});
-
-server.listen(PORT, 'localhost', function() {
-    console.log('Server start listen');
-});
+//root
+const root = ReactDOM.createRoot(document.getElementById('app'));
+const element2 = <Welcome name="Алиса" />;
+root.render(element2);
