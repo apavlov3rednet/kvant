16805e36a12fa1278bfd5a38e1012e36019f3405
alexxhub <a.pavlov@3red.net>
2024-02-14 12:05:17 +0300

    2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const ReactDOMClient = require('react-dom/client');
 
 //Топорный функционал JSX
 ReactDOM.render(<h1>Привет</h1>, document.getElementById('app'));
@@ -25,7 +26,59 @@ const element = <input placeholder='test' type='text' onClick={inputClick}/>;
 
 ReactDOM.render(element, app);
 
-//root
-const root = ReactDOM.createRoot(document.getElementById('app'));
+//root - new version
+const root = ReactDOMClient.createRoot(document.getElementById('app'));
 const element2 = <Welcome name="Алиса" />;
 root.render(element2);
+
+
+//components
+const Logo = () => {
+    return (
+        <img src='./public/images/logo.png'/>
+    );
+}
+
+class HeadText extends React.Component {
+    render() {
+        return(
+            <h1>{this.props.title}</h1>
+        )
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className='header'>
+                <Logo />
+                <HeadText title='SPA'/>
+                <menu>
+                <li data-route="owners">Владельцы</li>
+                <li data-route="brands">Бренды</li>
+                <li data-route="models">Модели</li>
+                <li data-route="cards">Карточки</li>
+                <li data-route="services">Услуги</li>
+                </menu>
+            </div>
+        );
+    }
+
+    // return (
+    //     <div className='header'>
+    //         <Logo />
+    //         <menu>
+    //         <li data-route="owners">Владельцы</li>
+    //         <li data-route="brands">Бренды</li>
+    //         <li data-route="models">Модели</li>
+    //         <li data-route="cards">Карточки</li>
+    //         <li data-route="services">Услуги</li>
+    //         </menu>
+    //     </div>
+    // );
+}
+
+root.render(<Header />);
+
+
+
